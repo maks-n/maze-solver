11be621b1220dc7ab073a89fd35e08603401bc67
maks-n <71183975+maks-n@users.noreply.github.com>
2025-05-18 22:21:00 -0400

    add draw line feature

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from graphics import Window
+from graphics import Window, Line, Point
+
 
 def main():
-    win = Window(800, 600)
+    win = Window(900, 600)
+    l = Line(Point(40, 70), Point(860, 530))
+    win.draw_line(l, "black")
     win.wait_for_close()
 
 
-if __name__ == "__main__":
-    main()
+main()
