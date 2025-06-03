c58c82f530484b22f3a798e4584852fe66e395be
maks-n <71183975+maks-n@users.noreply.github.com>
2025-06-03 14:30:40 -0400

    minor refactoring

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from graphics import Window, Line, Point
 
 
 def main():
-    win = Window(900, 600)
-    l = Line(Point(40, 70), Point(860, 530))
+    win = Window(800, 600)
+    l = Line(Point(40, 70), Point(760, 530))
     win.draw_line(l, "black")
     win.wait_for_close()
 
