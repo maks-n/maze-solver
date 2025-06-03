9603431c9660ac5c56956554d1091943c9018ebb
maks-n <71183975+maks-n@users.noreply.github.com>
2025-06-03 19:53:31 -0400

    add visited reset after first traversal

--- maze.py
+++ maze.py
@@ -20,6 +20,7 @@ class Maze:
         self.__create_cells()
         self.__break_entrance_and_exit()
         self.__break_walls_r(0, 0)
+        self.__reset_cells_visited()
 
     def __create_cells(self):
         for col in range (self.__num_cols):
@@ -93,3 +94,8 @@ class Maze:
                 self.__cells[col][row - 1].has_bottom_wall = False
             
             self.__break_walls_r(next_index[0], next_index[1])
+
+    def __reset_cells_visited(self):
+        for col in self.__cells:
+            for cell in col:
+                cell.visited = False
